fd06c037a24d5da57ae1291cd6042a0f071f944b
mirzakhanyaned <edmondik2@gmail.com>
2023-04-06 12:40:03 +0300

    Выводим сообщение с инструкцией по сделать ход и запрашиваем координаты

--- Project3/FileName.cpp
+++ Project3/FileName.cpp
@@ -39,3 +39,8 @@ int main()
         if ((i % 2) && (dgt == 1)) s = b;
         if ((i % 2) && (dgt == 2)) s = a;
     mylabel:
+        cout << "Сделайте ваш ход (" << s << ")" << endl;
+        cout << "Строка: ";
+        cin >> x;
+        cout << "Столбец: ";
+        cin >> y;
